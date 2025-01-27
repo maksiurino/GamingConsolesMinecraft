551fbe2bfe67e7039501375e004bdf1205d7972d
GamingConsoles <maksiurino@gmail.com>
2025-01-27 15:52:33 +0100

    a

--- src/main/java/net/maksiurino/gamingconsoles/block/ModBlocks.java
+++ src/main/java/net/maksiurino/gamingconsoles/block/ModBlocks.java
@@ -1,19 +1,8 @@
 package net.maksiurino.gamingconsoles.block;
 
 import net.maksiurino.gamingconsoles.GamingConsoles;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    public static final Block NINTENDO_SWITCH = new NintendoSwitch(AbstractBlock.Settings.create(Material.STONE));
-
-    public static void registerBlocks() {
-        Registry.register(Registries.BLOCK, new Identifier(GamingConsoles.MOD_ID, "nintendo_switch"), NINTENDO_SWITCH);
-    }
-
     public static void registerModBlocks() {
         GamingConsoles.LOGGER.info("Registering ModBlocks for " + GamingConsoles.MOD_ID);
     }
